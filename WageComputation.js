03ea36cf0f6647d93865f4b2f5374a5ca3ffb851
gorenm49 <gorenityanand96@gmail.com>
2022-03-21 15:57:37 +0530

    [Nityanand]:logic for check employee present or absent added

---
+++ WageComputation.js
@@ -0,0 +1,12 @@
+const IS_ABSENT =0;
+
+let checkStatus  = Math.floor(Math.random() * 10) % 2;
+
+if(checkStatus == IS_ABSENT)
+{
+    console.log("Employee is Absent!!");
+}
+else
+{
+    console.log("Employee is Present!!");
+}
