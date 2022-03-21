184bb068c4183e703c983585e5e12c21040d2fe0
gorenm49 <gorenityanand96@gmail.com>
2022-03-21 17:22:13 +0530

    [Nityanand]: logic to calculate emp wage  till total working days and hrs added

--- WageComputation.js
+++ WageComputation.js
@@ -1,4 +1,11 @@
 const IS_ABSENT =0;
+const IS_PART_TIME =1;
+const IS_FULL_TIME =2;
+const PART_TIME_HRS =4;
+const FULL_TIME_HRS =8;
+const WAGE_PER_HRS =20;
+const WORKING_DAYS_IN_MONTH = 20;
+const MAX_WORKING_HRS_IN_MONTH = 100;
 
 let checkStatus  = Math.floor(Math.random() * 10) % 2;
 
@@ -11,11 +18,6 @@ else
     console.log("Employee is Present!!");
 }
 
-const IS_PART_TIME =1;
-const IS_FULL_TIME =2;
-const PART_TIME_HRS =4;
-const FULL_TIME_HRS =8;
-const WAGE_PER_HRS =20;
 let emphr = 0;
 
 function retriveWorkingHrs(checkStatus)
@@ -38,12 +40,23 @@ emphr = retriveWorkingHrs(checkStatus)
 let empWage = emphr * WAGE_PER_HRS;
 console.log("Employee wage for a day is : "+empWage);
 
-const WORKING_DAYS_IN_MONTH = 20;
-
 for(let i = 0; i < WORKING_DAYS_IN_MONTH ; i++)
 {
     emphr += retriveWorkingHrs(checkStatus) ;
 }
 
 empWage = emphr * WAGE_PER_HRS;
-console.log("Total Work hrs :"+emphr+" Total Employee wage for a Month is : "+empWage);
+console.log("Total Work hrs : "+emphr+"  Total Employee wage for a Month is : "+empWage);
+
+
+
+let total_empHRs = 0;
+let total_workingDays = 0;
+
+while(total_empHRs <= MAX_WORKING_HRS_IN_MONTH && total_workingDays < WORKING_DAYS_IN_MONTH)
+{
+    total_workingDays++;
+    total_empHRs += retriveWorkingHrs(checkStatus);
+}
+empWage = total_empHRs * WAGE_PER_HRS;
+console.log("Total Working days: "+total_workingDays+"  Total working HRS :"+total_empHRs+"  Total Employee wage : "+empWage);
